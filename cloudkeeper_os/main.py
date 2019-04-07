84f1a1ce0879d1a674d75c334ad5e7469800a0c1
Taras Petrychkovych <petrytar@gmail.com>
2019-04-07 23:18:55 +0200

    Cosmetics

--- cloudkeeper_os/main.py
+++ cloudkeeper_os/main.py
@@ -4,7 +4,6 @@ Main entry point for Cloudkeeper-OS
 from cloudkeeper_os import configuration
 from cloudkeeper_os.grpc import server
 
-# poetry run cloudkeeper-os --openstack-identity-endpoint=http://147.251.253.3/identity --openstack-auth-type=v3password --openstack-username=demo --openstack-password=openstack --openstack-user-domain-name=default --openstack-project-name=demo --openstack-region-name=RegionOne  --openstack-project-name=demo -d
 def run():
     """
     Main method run for Cloudkeeper-OS
